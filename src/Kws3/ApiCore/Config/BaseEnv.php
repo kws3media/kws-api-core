0c02d629223b150fc63e492d020351190c12a95c
Sharif Ahmed <me.sharifahmed@gmail.com>
2021-03-09 21:59:13 +0600

    core base env class fix

--- src/Kws3/ApiCore/Config/BaseEnv.php
+++ src/Kws3/ApiCore/Config/BaseEnv.php
@@ -3,46 +3,34 @@ namespace Kws3\ApiCore\Config;
 
 class BaseEnv
 {
-  public $constents = [
-    'K_ENV' => 'local',
-    'IS_DEV' => true,
-    'K_ENV_PRODUCTION' => 'production',
-    'K_ENV_TESTING' => 'testing',
-    'K_ENV_DEV' => 'development',
-    'K_ENV_LOCAL' => 'local'
-  ];
 
-  public function run()
+  public function __construct()
   {
-    $this->setEnvionmentConstent();
-    $this->overwiteConstent();
-    $this->setAPIVersion();
-  }
-
-  //load environment variables as constants
-  protected function setEnvionmentConstent()
-  {
+    //load environment variables as constants
     foreach ($_SERVER as $key => $value) {
       if (strrpos($key, 'KWS_ENV_', -strlen($key)) !== false) { //starts with KWS_ENV_
-        $k = str_replace('KWS_ENV_', '', $key);
-        defined($k) or define($k, $value);
+          $k = str_replace('KWS_ENV_', '', $key);
+          defined($k) or define($k, $value);
       }
     }
-  }
 
-  protected function overwiteConstent()
-  {
-    foreach ($this->constents as $key => $value) {
-      defined($key) or define($key, $value);
-    }
-  }
+    defined('K_ENV') or define("K_ENV", "local");
 
-  protected function setAPIVersion()
-  {
+    /*
+    * ^This constant K_ENV^ is set to 'development' by default,
+    * This can be changed by setting environment variables in .htaccess
+    */
+
+    defined('IS_DEV') or define("IS_DEV", true);
+
+    defined('K_ENV_PRODUCTION') or define("K_ENV_PRODUCTION", "production");
+    defined('K_ENV_TESTING') or define("K_ENV_TESTING", "testing");
+    defined('K_ENV_DEV') or define("K_ENV_DEV", "development");
+    defined('K_ENV_LOCAL') or define("K_ENV_LOCAL", "local");
+
     //define the current API version in use
-    $request_uri  = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-    $uri_parts    = explode('/', $request_uri);
-
+    $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+    $uri_parts = explode('/', $request_uri);
     if (isset($uri_parts[1])) {
       $vnum = str_ireplace('v', '', $uri_parts[1]);
       define('API_VERSION', $vnum);
@@ -50,5 +38,10 @@ class BaseEnv
 
     //fallback in case version was not defined
     defined('API_VERSION') or define("API_VERSION", "undefined");
+  }
+
+  public function setENV($key, $value)
+  {
+    define($key, $value);
   }
 }
