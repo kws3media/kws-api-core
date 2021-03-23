a5e010cd10c6d447eba62ec6614b160591905c49
ekhaled <me.khaled@gmail.com>
2021-03-23 12:34:10 +0000

    define env constants

--- src/Framework.php
+++ src/Framework.php
@@ -2,6 +2,12 @@
 
 namespace Kws3\ApiCore;
 
+defined('K_ENV') or define("K_ENV", "local");
+defined('K_ENV_PRODUCTION') or define("K_ENV_PRODUCTION", "production");
+defined('K_ENV_TESTING') or define("K_ENV_TESTING", "testing");
+defined('K_ENV_DEV') or define("K_ENV_DEV", "development");
+defined('K_ENV_LOCAL') or define("K_ENV_LOCAL", "local");
+
 class Framework extends \Prefab
 {
 
