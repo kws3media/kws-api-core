30249cf7a6de41306ba366a2889a4a1534619ac7
Sharif Ahmed <me.sharifahmed@gmail.com>
2022-06-10 18:17:42 +0600

    DO driver fix

--- src/FS/DODriver.php
+++ src/FS/DODriver.php
@@ -4,16 +4,21 @@ namespace Kws3\ApiCore\FS;
 
 use \Aws\S3\S3Client;
 
-class DOSpacesDriver extends S3Driver
+class DODriver extends CloudDriver
 {
 
   public function __construct($opts = [])
   {
-    $this->opts = $opts;
-    $this->bucket = $this->opts['bucket'];
     if (empty($this->opts['region'])) {
       $this->opts['region'] = 'fra1';
     }
+    if (empty($this->opts['version'])) {
+      $this->opts['version'] = 'latest';
+    }
+    if (empty($this->opts['endpoint'])) {
+      $this->opts['endpoint'] = 'https://' . $this->opts['region'] . '.digitaloceanspaces.com';
+    }
+    parent::__construct($opts);
   }
 
   public function getUrl($fileObject)
@@ -24,24 +29,5 @@ class DOSpacesDriver extends S3Driver
   public function getFriendlyUrl($fileObject)
   {
     return 'https://' . $fileObject->bucket . '.' . $this->opts['region'] . '.digitaloceanspaces.com/' . implode('/', array_filter([$fileObject->folder, $fileObject->name]));
-  }
-
-  public function getS3()
-  {
-    if (!$this->s3) {
-
-      $this->_checkOpts();
-
-      $this->s3 = new S3Client([
-        'version' => 'latest',
-        'region' => $this->opts['region'],
-        'endpoint' => 'https://' . $this->opts['region'] . '.digitaloceanspaces.com',
-        'credentials' => [
-          'key'    => $this->opts['access_key'],
-          'secret' => $this->opts['secret'],
-        ]
-      ]);
-    }
-    return $this->s3;
   }
 }
